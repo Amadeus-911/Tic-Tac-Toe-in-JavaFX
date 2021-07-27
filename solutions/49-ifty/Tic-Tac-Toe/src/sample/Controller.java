6d73ea37bb7542988e48ac0732ea8413ab3fa446
Ifty <iftikharimrul@iut-dhaka.edu>
2021-07-27 20:36:02 +0600

    recognizing tile #5

--- solutions/49-ifty/Tic-Tac-Toe/src/sample/Controller.java
+++ solutions/49-ifty/Tic-Tac-Toe/src/sample/Controller.java
@@ -4,7 +4,36 @@ import javafx.fxml.FXML;
 
 public class Controller {
     @FXML
+
+
+
+
     public void  tile1(){
         System.out.println("Hello I'm Tile 1");
     }
+    public void  tile2(){
+        System.out.println("Hello I'm Tile 2");
+    }
+    public void  tile3(){
+        System.out.println("Hello I'm Tile 3");
+    }
+    public void  tile4(){
+        System.out.println("Hello I'm Tile 4");
+    }
+    public void  tile5(){
+        System.out.println("Hello I'm Tile 5");
+    }
+    public void  tile6(){
+        System.out.println("Hello I'm Tile 6");
+    }
+    public void  tile7(){
+        System.out.println("Hello I'm Tile 7");
+    }
+    public void  tile8(){
+        System.out.println("Hello I'm Tile 8");
+    }
+    public void  tile9(){
+        System.out.println("Hello I'm Tile 9");
+    }
+
 }
