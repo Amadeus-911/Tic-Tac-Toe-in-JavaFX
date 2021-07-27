d4b4acacaf32b4d6f7d1b244d872aa57f0a131d1
Ifty <iftikharimrul@iut-dhaka.edu>
2021-07-28 04:06:07 +0600

    random ai move added #8

--- solutions/49-ifty/Tic-Tac-Toe/src/sample/Controller.java
+++ solutions/49-ifty/Tic-Tac-Toe/src/sample/Controller.java
@@ -136,39 +136,7 @@ public class Controller {
                         System.out.println("default");
                         break;
                 }
-            }else{
-                int tile = randomTile();
-                System.out.println(tile);
-                switch (tile){
-                    case 1:
-                        System.out.println("do noting");
-                    case 2:
-                        ai.drawO(label2);
-                        System.out.println("draw");
-                    case 3:
-                        ai.drawO(label3);
-                        System.out.println("draw");
-                    case 4:
-                        ai.drawO(label4);
-                        System.out.println("draw");
-                    case 5:
-                        ai.drawO(label5);
-                        System.out.println("draw");
-                    case 6:
-                        ai.drawO(label6);
-                        System.out.println("draw");
-                    case 7:
-                        ai.drawO(label7);
-                        System.out.println("draw");
-                    case 8:
-                        ai.drawO(label8);
-                        System.out.println("draw");
-                    case 9:
-                        ai.drawO(label9);
-                        System.out.println("draw");
-                    default:
-                        System.out.println("default");
-                }
+                turnX = true;
             }
         }else{
             System.out.println("Not empty");
@@ -178,67 +146,401 @@ public class Controller {
 
     public void  tile2(){
         System.out.println("Hello I'm Tile 2");
-        Draw draw = new Draw(label2);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+//        Draw draw = new Draw(label2);
+//        if(turnX){
+//            draw.drawX();
+//            turnX = false;
+//        }
+        if(isEmpty(label2)){
+            if (turnX){
+                player.drawX(label2);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label1);
+                        break;
+                    case 2:
+                        System.out.println("do noting");
+                    case 3:
+                        ai.drawO(label3);
+                        break;
+                    case 4:
+                        ai.drawO(label4);
+                        break;
+                    case 5:
+                        ai.drawO(label5);
+                        break;
+                    case 6:
+                        ai.drawO(label6);
+                        break;
+                    case 7:
+                        ai.drawO(label7);
+                        break;
+                    case 8:
+                        ai.drawO(label8);
+                        break;
+                    case 9:
+                        ai.drawO(label9);
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
     }
     public void  tile3(){
-        System.out.println("Hello I'm Tile 3");
-        Draw draw = new Draw(label3);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+//        System.out.println("Hello I'm Tile 3");
+//        Draw draw = new Draw(label3);
+//        if(turnX){
+//            draw.drawX();
+//            turnX = false;
+//        }
+        if(isEmpty(label3)){
+            if (turnX){
+                player.drawX(label3);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label1);
+                        break;
+                    case 2:
+                        ai.drawO(label2);
+                        break;
+                    case 3:
+                        System.out.println("do noting");
+                    case 4:
+                        ai.drawO(label4);
+                        break;
+                    case 5:
+                        ai.drawO(label5);
+                        break;
+                    case 6:
+                        ai.drawO(label6);
+                        break;
+                    case 7:
+                        ai.drawO(label7);
+                        break;
+                    case 8:
+                        ai.drawO(label8);
+                        break;
+                    case 9:
+                        ai.drawO(label9);
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
     }
     public void  tile4(){
-        System.out.println("Hello I'm Tile 4");
-        Draw draw = new Draw(label4);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+        if(isEmpty(label4)){
+            if (turnX){
+                player.drawX(label4);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label1);
+                        break;
+                    case 2:
+                        ai.drawO(label2);
+                        break;
+                    case 3:
+                        ai.drawO(label3);
+                        break;
+                    case 4:
+                        System.out.println("do noting");
+                    case 5:
+                        ai.drawO(label5);
+                        break;
+                    case 6:
+                        ai.drawO(label6);
+                        break;
+                    case 7:
+                        ai.drawO(label7);
+                        break;
+                    case 8:
+                        ai.drawO(label8);
+                        break;
+                    case 9:
+                        ai.drawO(label9);
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
     }
     public void  tile5(){
         System.out.println("Hello I'm Tile 5");
-        Draw draw = new Draw(label5);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+//        Draw draw = new Draw(label5);
+//        if(turnX){
+//            draw.drawX();
+//            turnX = false;
+//        }
+        if(isEmpty(label5)){
+            if (turnX){
+                player.drawX(label5);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label1);
+                        break;
+
+                    case 2:
+                        ai.drawO(label2);
+                        break;
+                    case 3:
+                        ai.drawO(label3);
+                        break;
+                    case 4:
+                        ai.drawO(label4);
+                        break;
+                    case 5:
+                        System.out.println("do noting");
+                    case 6:
+                        ai.drawO(label6);
+                        break;
+                    case 7:
+                        ai.drawO(label7);
+                        break;
+                    case 8:
+                        ai.drawO(label8);
+                        break;
+                    case 9:
+                        ai.drawO(label9);
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
     }
     public void  tile6(){
         System.out.println("Hello I'm Tile 6");
-        Draw draw = new Draw(label6);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+//        Draw draw = new Draw(label6);
+//        if(turnX){
+//            draw.drawX();
+//            turnX = false;
+//        }
+        if(isEmpty(label6)){
+            if (turnX){
+                player.drawX(label6);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label1);
+                        break;
+
+                    case 2:
+                        ai.drawO(label2);
+                        break;
+                    case 3:
+                        ai.drawO(label3);
+                        break;
+                    case 4:
+                        ai.drawO(label4);
+                        break;
+                    case 5:
+                        ai.drawO(label5);
+                        break;
+                    case 6:
+                        System.out.println("do noting");
+                    case 7:
+                        ai.drawO(label7);
+                        break;
+                    case 8:
+                        ai.drawO(label8);
+                        break;
+                    case 9:
+                        ai.drawO(label9);
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
     }
     public void  tile7(){
         System.out.println("Hello I'm Tile 7");
-        Draw draw = new Draw(label7);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+//        Draw draw = new Draw(label7);
+//        if(turnX){
+//            draw.drawX();
+//            turnX = false;
+//        }
+        if(isEmpty(label7)){
+            if (turnX){
+                player.drawX(label7);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label1);
+                        break;
+
+                    case 2:
+                        ai.drawO(label2);
+                        break;
+                    case 3:
+                        ai.drawO(label3);
+                        break;
+                    case 4:
+                        ai.drawO(label4);
+                        break;
+                    case 5:
+                        ai.drawO(label5);
+                        break;
+                    case 6:
+                        ai.drawO(label6);
+                        break;
+                    case 7:
+                        System.out.println("do noting");
+                    case 8:
+                        ai.drawO(label8);
+                        break;
+                    case 9:
+                        ai.drawO(label9);
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
     }
     public void  tile8(){
         System.out.println("Hello I'm Tile 8");
-        Draw draw = new Draw(label8);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+//        Draw draw = new Draw(label8);
+//        if(turnX){
+//            draw.drawX();
+//            turnX = false;
+//        }
+        if(isEmpty(label8)){
+            if (turnX){
+                player.drawX(label8);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label1);
+                        break;
+                    case 2:
+                        ai.drawO(label2);
+                        break;
+                    case 3:
+                        ai.drawO(label3);
+                        break;
+                    case 4:
+                        ai.drawO(label4);
+                        break;
+                    case 5:
+                        ai.drawO(label5);
+                        break;
+                    case 6:
+                        ai.drawO(label6);
+                        break;
+                    case 7:
+                        ai.drawO(label7);
+                        break;
+                    case 8:
+                        System.out.println("do noting");
+                    case 9:
+                        ai.drawO(label9);
+                        break;
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
     }
     public void  tile9(){
         System.out.println("Hello I'm Tile 9");
-        Draw draw = new Draw(label9);
-        if(turnX){
-            draw.drawX();
-            turnX = false;
+//        Draw draw = new Draw(label9);
+//        if(turnX){
+//            draw.drawX();
+//            turnX = false;
+//        }
+        if(isEmpty(label9)){
+            if (turnX){
+                player.drawX(label9);
+                turnX = false;
+                int tile = randomTile();
+                System.out.println(tile);
+                switch (tile){
+                    case 1:
+                        ai.drawO(label9);
+                        break;
+                    case 2:
+                        ai.drawO(label2);
+                        break;
+                    case 3:
+                        ai.drawO(label3);
+                        break;
+                    case 4:
+                        ai.drawO(label4);
+                        break;
+                    case 5:
+                        ai.drawO(label5);
+                        break;
+                    case 6:
+                        ai.drawO(label6);
+                        break;
+                    case 7:
+                        ai.drawO(label7);
+                        break;
+                    case 8:
+                        ai.drawO(label8);
+                        break;
+                    case 9:
+                        System.out.println("do noting");
+                    default:
+                        System.out.println("default");
+                        break;
+                }
+                turnX = true;
+            }
+        }else{
+            System.out.println("Not empty");
         }
+
     }
 
 }
